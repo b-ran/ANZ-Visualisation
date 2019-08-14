3557145f2230c64b8a48d1e53bce1adb40665c33
scotthbran <scotthbran@ecs.vuw.ac.nz>
2019-08-14 16:00:13 +1200

    setup d3

--- src/app/components/bubble-vis/bubble-vis.component.ts
+++ src/app/components/bubble-vis/bubble-vis.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {DataManagerService} from '../../services/data-manager.service';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-bubble-vis',
-  templateUrl: './bubble-vis.component.html',
+  template: `
+    <svg width="25vw" height="25vh"
+         viewBox="0 0 800 800"></svg>`,
   styleUrls: ['./bubble-vis.component.css']
 })
 export class BubbleVisComponent implements OnInit {
 
-  constructor() {
+  private svg: any;
+
+  constructor(private container: ElementRef) {
     const df = new DataManagerService((result) => console.log(result));
   }
 
   ngOnInit() {
   }
+
+
+  private initSvg() {
+    this.svg = d3.select(this.container.nativeElement).select('svg');
+  }
+
 
 }
