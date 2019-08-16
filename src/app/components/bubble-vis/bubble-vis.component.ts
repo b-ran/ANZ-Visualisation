661ee7650d0a6d64b96eac2cdca9fdae817114de
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2019-08-17 04:54:16 +1200

    Add basic bubble chart component

--- src/app/components/bubble-vis/bubble-vis.component.ts
+++ src/app/components/bubble-vis/bubble-vis.component.ts
@@ -1,110 +1,89 @@
+/* tslint:disable:semicolon */
 import {Component, ElementRef, OnInit} from '@angular/core';
 import {DataManagerService} from '../../services/data-manager.service';
 import * as d3 from 'd3/dist/d3.min.js';
-import { $ } from 'protractor';
 
 @Component({
   selector: 'app-bubble-vis',
   template: `
-    <svg width="25vw" height="25vh"
-         viewBox="0 0 800 800"></svg>`,
+    <svg viewBox="0 0 800 800"></svg>`,
   styleUrls: ['./bubble-vis.component.css']
 })
 export class BubbleVisComponent implements OnInit {
 
   private svg: any;
   private data: any;
-  private ratioHome = function (d) { return (d.homeWins / d.homeGames)};
+  private ratioHome = (d) => {
+    return (d.homeWins / d.homeGames);
+  };
 
   constructor(private container: ElementRef) {
-    const df = new DataManagerService((result) => this.data = result);
+    const df = new DataManagerService((result) => this.data = {children: Array.from(result.values())});
+    console.log(this.data);
   }
 
   ngOnInit() {
-    this.initSvg()
+    this.initSvg();
   }
-
 
   private initSvg() {
-    this.svg = d3.select(this.container.nativeElement).select('svg');
-    this.data.forEach(element => {
-      this.svg.append("teams")
-              .attr("teamName", element.teamName)
-              .attr("homeWins", element.homeWins)
-              .attr("homeGames", element.homeGames)
-              .attr("awayWins", element.awayWins)
-              .attr("awayGames", element.awayGames);
-    });
-    
-    var diameter = 600;
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
-    
+    const diameter = 300;
+    const bubble = d3.pack(this.data)
+      .size([diameter, diameter])
+      .padding(1.5);
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+    this.svg = d3.select(this.container.nativeElement).select('svg').attr('class', 'bubble');
 
-    var bubble = d3.pack(this.data)
-                    .size([diameter, diameter])
-                    .padding(1.5);
-    
+    const nodes = d3.hierarchy(this.data)
+      .sum((d) => {
+        return d.homeWins;
+      });
 
+    const node = this.svg.selectAll('.node')
+      .data(bubble(nodes).descendants())
+      .enter()
+      .filter((d) => {
+        return !d.children;
+      })
+      .append('g')
+      .attr('class', 'node')
+      .attr('transform', (d) => {
+        return `translate(${d.x}, ${d.y})`;
+      });
 
-    var nodes = d3.hierarchy(this.data)
-            .sum(function (d) { return (d.homeWins / d.homeGames)});
-  
-    
-    var node = this.svg.selectAll("teams")
-                  .append("g")
-                  .attr("class", "node")
-                  .attr("transform", function(d){
-                    return "translate(" + d.x + "," + d.y + ")"
-                  });
+    node.append('circle')
+      .attr('r', (d) => {
+        return d.r;
+      })
+      .style('fill', (d, i) => {
+        return color(i);
+      });
 
-    node.append("title")
-        .text(function(d){
-          return d.teamName +  ":" + (d.homeWins / d.homeGames);
-        });
+    node.append('text')
+      .attr('dy', '.2em')
+      .style('text-anchor', 'middle')
+      .text((d) => {
+        return d.data.teamName;
+      })
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', (d) => {
+        return d.r / 7;
+      })
+      .attr('fill', 'white');
 
-    node.append("circle")
-        .attr("r", function(d){
-          d.r;
-        })
-        .style("fill", function(d,i){
-          return color(i);
-        });
-  
+    node.append('text')
+      .attr('dy', '1.3em')
+      .style('text-anchor', 'middle')
+      .text((d) => {
+        return 'Home Wins: ' + d.data.homeWins;
+      })
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', (d) => {
+        return d.r / 7;
+      })
+      .attr('fill', 'white');
 
-
-        node.append("text")
-        .attr("dy", ".2em")
-        .style("text-anchor", "middle")
-        .text(function(d) {
-            return d.teamName;
-        })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", function(d){
-            return d.r/5;
-        })
-        .attr("fill", "white");
-
-    node.append("text")
-        .attr("dy", "1.3em")
-        .style("text-anchor", "middle")
-        .text(function(d) {
-            return (d.homeWins / d.homeGames);
-        })
-        .attr("font-family",  "Gill Sans", "Gill Sans MT")
-        .attr("font-size", function(d){
-            return d.r/5;
-        })
-        .attr("fill", "white");
-
-    d3.select(self.frameElement)
-        .style("height", diameter + "px");
-    
   }
-
-
-  private displayData(){
-    
-    }
 
 
 }
