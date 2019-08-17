297e53571e43190d721b5e35eb8874208de2b0ad
Andrew Hay <hayandr1@ecs.vuw.ac.nz>
2019-08-18 09:22:24 +1200

    Added 'interCountryWins' and 'interCountryGames' to returned JSONs

--- src/app/services/data-manager.service.ts
+++ src/app/services/data-manager.service.ts
@@ -3,22 +3,27 @@ import {Injectable} from '@angular/core';
 
 // @ts-ignore
 import data from '../../assets/clean-data/anz-championship-cleaned-data.json';
+import teamInfo from '../../assets/clean-data/team-venue-country.json';
 
-const teamInfo = () => {
-  const result = new Map();
-  data.forEach((element) => {
-    if (!result.has(element['Home Team'])) {
-      result.set(element['Home Team'], element.Venue);
-    }
-  });
-  return result;
-};
+// const teamInfo = () => {
+//   const result = new Map();
+//   data.forEach((element) => {
+//     if (!result.has(element['Home Team'])) {
+//       result.set(element['Home Team'], element.Venue);
+//     }
+//   });
+//   return result;
+// };
 
 function accumulateValues(input) {
   const result = new Map();
   input.forEach((element) => {
     const homeResult = {team: element['Home Team'], outcome: 0};
     const awayResult = {team: element['Away Team'], outcome: 0};
+
+    const isInterCountry = (teamInfo.find((info) => info.team === element['Home Team']).country
+    !== teamInfo.find((info) => info.team === element['Away Team']).country);
+
     if (element['Home Score'] > element['Away Score']) {
       homeResult.outcome = 1;
       awayResult.outcome = 0;
@@ -31,22 +36,28 @@ function accumulateValues(input) {
       const vals = result.get(homeResult.team);
       vals.homeWins += homeResult.outcome;
       vals.homeGames += 1;
+      vals.interCountryGames += isInterCountry ? homeResult.outcome : 0;
+      vals.interCountryGames += isInterCountry ? 1 : 0;
       result.set(homeResult.team, vals);
     } else {
       result.set(homeResult.team,
         {
           teamName: homeResult.team, homeWins: homeResult.outcome, homeGames: 1, awayWins: 0, awayGames: 0,
+          interCountryWins: isInterCountry ? homeResult.outcome : 0, interCountryGames: isInterCountry ? 1 : 0
         });
     }
     if (result.has(awayResult.team)) {
       const vals = result.get(awayResult.team);
       vals.awayWins += awayResult.outcome;
       vals.awayGames += 1;
+      vals.interCountryWins += isInterCountry ? awayResult.outcome : 0;
+      vals.interCountryGames += isInterCountry ? 1 : 0;
       result.set(awayResult.team, vals);
     } else {
       result.set(awayResult.team,
         {
           teamName: awayResult.team, homeWins: 0, homeGames: 0, awayWins: awayResult.outcome, awayGames: 1,
+          interCountryWins: isInterCountry ? awayResult.outcome : 0, interCountryGames: isInterCountry ? 1 : 0
         });
     }
   });
