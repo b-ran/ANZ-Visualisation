a168975df367956c53d2bd9db60f749b4d5530ab
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2019-08-19 22:50:04 +1200

    Add date range label to slider

--- src/app/components/range-slider/range-slider.component.ts
+++ src/app/components/range-slider/range-slider.component.ts
@@ -11,13 +11,17 @@ import * as d3 from 'd3/dist/d3.min.js';
 export class RangeSliderComponent implements OnInit {
 
   private datesRange: any;
+  private leftDate: any;
+  private rightDate: any;
 
   constructor(private rangeSlider: PlayRangeService, private dataManager: DataManagerService) {
     const dateRange = dataManager.dateRange();
     this.datesRange = d3.timeDays(dateRange.startDate, dateRange.endDate);
     this.rangeSlider.createD3RangeSlider(0, this.datesRange.length - 1, 100);
     this.rangeSlider.onChange((newRange) => {
-      dataManager.setDateFilter(this.datesRange[newRange.begin], this.datesRange[newRange.end]);
+      this.leftDate = this.datesRange[newRange.begin];
+      this.rightDate = this.datesRange[newRange.end];
+      dataManager.setDateFilter(this.leftDate, this.rightDate);
     });
   }
 
