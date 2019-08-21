daed3cac2d9af3091a6132f4408ad2f19121cb48
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2019-08-21 21:44:46 +1200

    Add recreation system for slider

--- src/app/components/range-slider/range-slider.component.ts
+++ src/app/components/range-slider/range-slider.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PlayRangeService} from 'play-range';
 import {DataManagerService} from '../../services/data-manager.service';
 import * as d3 from 'd3/dist/d3.min.js';
@@ -15,17 +15,24 @@ export class RangeSliderComponent implements OnInit {
   private rightDate: any;
 
   constructor(private rangeSlider: PlayRangeService, private dataManager: DataManagerService) {
-    const dateRange = dataManager.dateRange();
-    this.datesRange = d3.timeDays(dateRange.startDate, dateRange.endDate);
-    this.rangeSlider.createD3RangeSlider(0, this.datesRange.length - 1, 100);
-    this.rangeSlider.onChange((newRange) => {
-      this.leftDate = this.datesRange[newRange.begin];
-      this.rightDate = this.datesRange[newRange.end];
-      dataManager.setDateFilter(this.leftDate, this.rightDate);
+    this.recreateSlider(this.dataManager.dateRange().startDate, this.dataManager.dateRange().endDate);
+    dataManager.setSliderCallback((startDate, endDate) => {
+      this.recreateSlider(startDate, endDate);
     });
   }
 
   ngOnInit() {
+  }
+
+  recreateSlider(startDate: Date, endDate: Date) {
+    d3.select('#slider-container').remove();
+    this.datesRange = d3.timeDays(startDate, endDate);
+    this.rangeSlider.createD3RangeSlider(0, this.datesRange.length - 1, 100);
+    this.rangeSlider.onChange((newRange) => {
+      this.leftDate = this.datesRange[newRange.begin];
+      this.rightDate = this.datesRange[newRange.end];
+      this.dataManager.setDateFilter(this.leftDate, this.rightDate);
+    });
   }
 
 }
